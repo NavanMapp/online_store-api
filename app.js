ad5bfeeb770382f28509c73a9f728acdbf394b4c
NavMapp <95779113+NavanMapp@users.noreply.github.com>
2024-02-24 18:50:27 +0200

    app and server handler

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-require('dot.env').config()
-
+require('dotenv').config()
 const express = require('express')
 const app = express()
+
+
+app.get((req, res, next) => {
+    console.log('Request received!')
+    next()
+})
+
+app.use((req, res, next) => {
+    res.status(201)
+    next()
+})
+
+app.use((req, res, next) => {
+    res.send({ message: 'Your request was successful!' })
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log('Response sent successfully!');
+})
 
 module.exports = app
--- server.js
+++ server.js
@@ -1,13 +1,58 @@
 require('dotenv').config()
-
-const PORT = require('process.env.PORT')
-const http = require('http')
+const PORT = process.env.PORT
 const app = require('./app')
+const http = require('http')
+
+const normalizePort = val => {
+    const port = parseInt(val, 10)
+
+    if (isNaN(port)) {
+        return val
+    }
+
+    if (port >= 0) {
+        return port
+    }
+
+    return false
+}
+
+const port = normalizePort(PORT)
 
 app.set('port', PORT)
 
-const server = http.createServer((req, res) => {
-    res.end('This is my server respose!')
+const errorHandler = error => {
+
+    if (error.syscal !== 'listen') {
+        throw error
+    }
+
+    const address = server.address()
+    const bind = typeof address === 'string' ? 'pipe' + address : 'port: ' + port
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.')
+            process.exit(1)
+            break
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.')
+            process.exit(1)
+            break
+        default:
+            throw error
+    }
+}
+
+const server = http.createServer(app)
+
+server.on('error', errorHandler)
+
+server.on('listening', () => {
+    const address = server.address()
+
+    const bind = typeof address === 'string' ? 'pipe ' + address: 'port ' + port
+    console.log('listening on ' + bind) 
 })
 
-server.listen(PORT, console.log(`Listening from port ${PORT}`)) 
+server.listen(PORT)
