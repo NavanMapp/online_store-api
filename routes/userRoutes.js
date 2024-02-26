e7422afb4fec9326fdb846086dcb18fdefea7b76
Návan <navanmapp@gmail.com>
2024-02-26 15:55:21 +0200

    app file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+router.route('/auth/login').post((req, res) => {
+    res.send('User found on database, login in underway!')
+})
+
+router.route('/auth/signin').get((req,res) => {
+    res.send('got username and password, now saving it to database!')
+})
+
+
+
 module.exports = router
