b2600362cfc8c755c96b6e14914758ace9ff8ab1
NavMapp <95779113+NavanMapp@users.noreply.github.com>
2024-02-29 21:40:49 +0200

    Models, middleware, routes

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 require('dotenv').config()
 const express = require('express')
+const mongoURL = process.env.mongoURL
+const mongoose = require('mongoose')
 const router = require('./routes/userRoutes.js')
 const app = express()
 
+
+mongoose.connect(mongoURL)
+    .then(() => {
+        console.log('Successfully connected to mongoDB atlast')
+    }).catch((error) => {
+        console.error(error)
+    })
 
 router.get((req, res, next) => {
     console.log('Request received!')
